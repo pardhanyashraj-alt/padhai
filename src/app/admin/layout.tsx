f0a61afb7ca33d9084f6df14b9d62ba98e4e15bf
hybs123 <haidersoni47@gmail.com>
2026-04-03 19:17:18 +0530

    check-build

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { AdminProvider } from "../../context/AdminContext";
+
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+  return <AdminProvider>{children}</AdminProvider>;
 }
